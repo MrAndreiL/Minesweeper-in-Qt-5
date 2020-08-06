d371e72354b20b8b57715fbbbffcbef44812ef6e
MrAndreiL <lunguandrei759@gmail.com>
2020-08-06 17:33:43 +0300

    Button

--- button.cpp
+++ button.cpp
@@ -6,6 +6,7 @@ Button::Button(QWidget *parent, const char *name):QWidget(parent, 0)
    button = new QPushButton(this);
    button->setFont(QFont("Times", 18, QFont::Bold));
    button->setGeometry(0, 0, width(), height());
+   button->setStyleSheet("background-image: url(:/resources/img/button.png)");
    connect(button, SIGNAL(clicked()), SIGNAL(clicked()));
    connect(button, SIGNAL(clicked()), SLOT(changeState()));
 }
@@ -13,6 +14,5 @@ Button::Button(QWidget *parent, const char *name):QWidget(parent, 0)
 void Button::changeState()
 {
    state = 1;
-   //button->setVisible(false);
-   button->setStyleSheet("background-image: url(:/resources/img/bomb.jpg)");
+   button->setVisible(false);
 }
