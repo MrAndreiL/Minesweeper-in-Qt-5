4b96eec8a211b228f9efc85afd9d6ce8257ec23e
MrAndreiL <lunguandrei759@gmail.com>
2020-08-06 22:39:15 +0300

    Game functionality

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ private:
   Button *values[9][9];
   qint32 table[9][9];
   void getRandomBombs(qint32 table[9][9], qint32 bombs);
+  void setBoardValues(qint32 table[9][9]);
 };
 
 Minesweeper::Minesweeper(QWidget *parent, const char *name, qint32 bombs):QWidget(parent, Qt::Window)
@@ -48,14 +49,45 @@ Minesweeper::Minesweeper(QWidget *parent, const char *name, qint32 bombs):QWidge
    labels[2]->move(400, 25);
    labels[2]->setFont(QFont("Times", 13, QFont::Bold));
 
-   qint32 startx = 40;
-   qint32 starty = 30;
+   const qint32 startx = 40;
+   const qint32 starty = 30;
+
    getRandomBombs(table, bombs);
+   setBoardValues(table);
+
    for (qint32 i = 0; i < 9; i++)
-       for (qint32 j = 0; j < 9; j++) {
-         elements[i][j] = new QLabel(this);
-         elements[i][j]->setGeometry(startx + (j + 1) * 40, starty + (i + 1) * 30, 35, 30);
-         elements[i][j]->setStyleSheet("background-image: url(:/resources/img/bomb.jpg)");
+     for (qint32 j = 0; j < 9; j++) {
+       elements[i][j] = new QLabel(this);
+       elements[i][j]->setGeometry(startx + (j + 1) * 40, starty + (i + 1) * 30, 35, 30);
+       if (table[i][j] == -1)
+          elements[i][j]->setStyleSheet("background-image: url(:/resources/img/bomb.jpg)");
+       else
+           switch (table[i][j]) {
+               case 1:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/1.png)");
+                 break;
+               case 2:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/2.png)");
+                 break;
+               case 3:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/3.png)");
+                 break;
+               case 4:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/4.png)");
+                 break;
+               case 5:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/5.png)");
+                 break;
+               case 6:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/6.png)");
+                 break;
+               case 7:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/7.png)");
+                 break;
+               case 8:
+                 elements[i][j]->setStyleSheet("background-image: url(:/resources/img/8.png)");
+                 break;
+           }
        }
 
    // Draw the main board.
@@ -77,10 +109,26 @@ void Minesweeper::getRandomBombs(qint32 table[9][9], qint32 bombs)
        qint32 i = QRandomGenerator::global()->bounded(0, 8);
        qint32 j = QRandomGenerator::global()->bounded(0, 8);
        if (table[i][j] == 0) {
-           table[i][j] = 1;
+           table[i][j] = -1;
            bombs--;
        }
    }
+}
+
+void Minesweeper::setBoardValues(qint32 table[9][9])
+{
+   qint32 tokens_i[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+   qint32 tokens_j[8] = {-1,  0,  1,-1, 1,-1, 0, 1};
+   for (qint32 i = 0; i < 9; i++)
+      for (qint32 j = 0; j < 9; j++)
+          if (table[i][j] == 0)
+              for (qint32 k = 0; k < 8; k++) {
+                  qint32 newi = i + tokens_i[k];
+                  qint32 newj = j + tokens_j[k];
+                  if ((0 <= newi && newi <= 9) && (0 <= newj && newj <= 9))
+                      if (table[newi][newj] == -1)
+                          table[i][j]++;
+              }
 }
 
 int main(int argc, char **argv)
